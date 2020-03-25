79b661948afe8c7e35caee29d590ba717a3e52a1
Jeremy Volkman <jeremy@jvolkman.com>
2020-03-25 14:21:35 -0700

    Overwrite files if they exist

--- core/src/main/resources/update_include_files.sh
+++ core/src/main/resources/update_include_files.sh
@@ -6,5 +6,5 @@ mkdir -p "${DIR}/include/google/protobuf"
 cd "${DIR}/include/google/protobuf"
 
 for file in any api descriptor duration empty field_mask source_context struct timestamp type wrappers; do
-  wget "https://raw.githubusercontent.com/protocolbuffers/protobuf/master/src/google/protobuf/${file}.proto"
+  wget "https://raw.githubusercontent.com/protocolbuffers/protobuf/master/src/google/protobuf/${file}.proto" -O "${file}.proto"
 done
