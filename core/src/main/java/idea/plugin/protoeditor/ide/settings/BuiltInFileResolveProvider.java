3aed11b9c90fbd731e33c166731206fd835617b3
wz <wangzheng@bybutter.com>
2020-07-21 11:41:50 +0800

    Add built-in filter resolver for non-java project

--- core/src/main/java/idea/plugin/protoeditor/ide/settings/BuiltInFileResolveProvider.java
+++ core/src/main/java/idea/plugin/protoeditor/ide/settings/BuiltInFileResolveProvider.java
@@ -2,8 +2,12 @@ package idea.plugin.protoeditor.ide.settings;
 
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.projectRoots.JavaSdkType;
+import com.intellij.openapi.projectRoots.Sdk;
+import com.intellij.openapi.projectRoots.SdkTypeId;
 import com.intellij.openapi.roots.ModuleRootManager;
 import com.intellij.openapi.roots.OrderRootType;
+import com.intellij.openapi.roots.ProjectRootManager;
 import com.intellij.openapi.roots.libraries.Library;
 import com.intellij.openapi.roots.libraries.LibraryTablesRegistrar;
 import com.intellij.openapi.vfs.VfsUtil;
@@ -13,17 +17,25 @@ import idea.plugin.protoeditor.lang.resolve.FileResolveProvider;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.net.URL;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
-public class LibrariesAndSdkClassesRootsFileResolveProvider implements FileResolveProvider {
+public class BuiltInFileResolveProvider implements FileResolveProvider {
+  private final VirtualFile includeDir = VfsUtil.findFileByURL(BuiltInFileResolveProvider.class.getResource("/include"));
+
   @Nullable
   @Override
   public VirtualFile findFile(@NotNull String path, @NotNull Project project) {
-    for (Library library : LibraryTablesRegistrar.getInstance().getLibraryTable(project).getLibraries()) {
-      VirtualFile result = findFileInRoots(path, library.getFiles(OrderRootType.CLASSES));
-      if(result != null) return result;
+    if (includeDir == null) return null;
+    if (!checkInJavaEnv(project)) {
+      VirtualFile file = includeDir.findFileByRelativePath(path);
+      if (file != null && file.exists()) {
+        return file;
+      }
     }
     return null;
   }
@@ -31,24 +43,23 @@ public class LibrariesAndSdkClassesRootsFileResolveProvider implements FileResol
   @Nullable
   @Override
   public VirtualFile findFile(@NotNull String path, @NotNull Module module) {
-    VirtualFile[] roots = ModuleRootManager.getInstance(module).orderEntries().getAllLibrariesAndSdkClassesRoots();
-    return findFileInRoots(path, roots);
+    return findFile(path, module.getProject());
   }
 
   @NotNull
   @Override
   public Collection<ChildEntry> getChildEntries(@NotNull String path, @NotNull Project project) {
-    VirtualFile[] roots = Arrays.stream(LibraryTablesRegistrar.getInstance().getLibraryTable(project).getLibraries())
-        .flatMap(library -> Arrays.stream(library.getFiles(OrderRootType.CLASSES)))
-        .collect(Collectors.toList()).toArray(new VirtualFile[]{});
-    return findChildEntriesInRoots(roots);
+    if (includeDir == null) return Collections.emptyList();
+    if (!checkInJavaEnv(project)) {
+      return findChildEntriesInRoot(includeDir);
+    }
+    return Collections.emptyList();
   }
 
   @NotNull
   @Override
   public Collection<ChildEntry> getChildEntries(@NotNull String path, @NotNull Module module) {
-    VirtualFile[] roots = ModuleRootManager.getInstance(module).orderEntries().getAllLibrariesAndSdkClassesRoots();
-    return findChildEntriesInRoots(roots);
+    return getChildEntries(path, module.getProject());
   }
 
   @Nullable
@@ -66,22 +77,20 @@ public class LibrariesAndSdkClassesRootsFileResolveProvider implements FileResol
   @NotNull
   @Override
   public GlobalSearchScope getSearchScope(@NotNull Project project) {
-    return GlobalSearchScope.everythingScope(project);
+    return GlobalSearchScope.projectScope(project);
   }
 
-  private VirtualFile findFileInRoots(String path, VirtualFile[] roots) {
-    for (VirtualFile root : roots) {
-      VirtualFile file = root.findFileByRelativePath(path);
-      if (file != null && file.exists()) {
-        return file;
-      }
-    }
-    return null;
+  private boolean checkInJavaEnv(@NotNull Project project) {
+    Sdk projectSdk = ProjectRootManager.getInstance(project).getProjectSdk();
+    if (projectSdk == null) return false;
+    SdkTypeId projectSdkId = projectSdk.getSdkType();
+    return projectSdkId instanceof JavaSdkType;
   }
 
-  private Collection<ChildEntry> findChildEntriesInRoots(VirtualFile[] roots) {
-    return Arrays.stream(roots).flatMap(root -> VfsUtil.getChildren(root, PROTO_AND_DIRECTORY_FILTER)
+  private Collection<ChildEntry> findChildEntriesInRoot(VirtualFile root) {
+    return VfsUtil.getChildren(root, PROTO_AND_DIRECTORY_FILTER)
         .stream()
-        .map(child -> new ChildEntry(child.getName(), child.isDirectory()))).collect(Collectors.toList());
+        .map(child -> new ChildEntry(child.getName(), child.isDirectory()))
+        .collect(Collectors.toList());
   }
 }
--- core/src/main/java/idea/plugin/protoeditor/ide/settings/DefaultConfigurator.java
+++ core/src/main/java/idea/plugin/protoeditor/ide/settings/DefaultConfigurator.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,17 +16,13 @@
 package idea.plugin.protoeditor.ide.settings;
 
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.project.ProjectManager;
-import com.intellij.openapi.roots.ModuleRootManager;
 import com.intellij.openapi.roots.ProjectRootManager;
-import com.intellij.openapi.roots.ex.ProjectRootManagerEx;
 import com.intellij.openapi.vfs.VfsUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import idea.plugin.protoeditor.ide.settings.PbProjectSettings.ImportPathEntry;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.net.URL;
 import java.util.Collection;
 import java.util.Collections;
 
